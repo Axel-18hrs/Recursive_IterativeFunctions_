7500678e08abac0e29101ebd483656902eb5816e
Axel Gael Carlin Guzman <axel.18hrs@gmail.com>
2023-08-24 16:58:34 -0600

    Proyect - Unit 1

---
+++ main.py
@@ -0,0 +1,25 @@
+# Function to calculate factorial iteratively
+def iterative_factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+# Function to calculate factorial recursively
+def recursive_factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * recursive_factorial(n - 1)
+
+choice = int(input("// Choose an option:" +
+      "\n[1]: iterative factorial" +
+      "\n[2]: recursive factorial\n"))
+
+if choice == 1:
+    num = int(input("Enter a number: "))
+    print(iterative_factorial(num))
+
+if choice == 2:
+    num = int(input("Enter a number: "))
+    print(recursive_factorial(num))
